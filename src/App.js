729896b078cc0728deb3958577e9e9f9e879963e
rickydia <rickydia@umich.edu>
2018-07-19 23:32:44 -0700

    added build and made _target='blank' safer?

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ class App extends Component {
 				<p>I started playing soccer when I was about four.</p>
 				<p>I am not that great at it anymore, but I am still absolutely in love with the sport.</p>
 				<p>I am also in love with both movies and photography.</p>
-				<p>I sometimes take <a href = "https://photos.app.goo.gl/T57dzmY4i4XSOfyO2" target="_blank">pictures</a>.</p>
+				<p>I sometimes take <a href = "https://photos.app.goo.gl/T57dzmY4i4XSOfyO2" target="_blank" rel="noopener noreferrer">pictures</a>.</p>
 				<p>I now go to the University of Michigan, where I'm learning computer science.</p>
-				<p>I write <a href = "https://www.github.com/rickydia" target="_blank">code</a> sometimes.</p>
-				<p>I joined the world's first professional technology fraternity, <a href = "https://kappathetapi.com" target="_blank">Kappa Theta Pi</a>.</p>
+				<p>I write <a href = "https://www.github.com/rickydia" target="_blank" rel="noopener noreferrer">code</a> sometimes.</p>
+				<p>I joined the world's first professional technology fraternity, <a href = "https://kappathetapi.com" target="_blank" rel="noopener noreferrer">Kappa Theta Pi</a>.</p>
 				<p>I worked with a team of ten made that website.</p>
-				<p>I have been working with <a href = "https://halthungerinitiative.weebly.com" target="_blank">Halt Hunger Initiative</a>, which is an amazing charity.</p>
+				<p>I have been working with <a href = "https://halthungerinitiative.weebly.com" target="_blank" rel="noopener noreferrer">Halt Hunger Initiative</a>, which is an amazing charity.</p>
 				<p>I am currently looking for an internship in software development for summer 2019.</p>
-				<p>I have a more detailed <a href="resume.pdf" target="_blank">résumé</a> if you want to look.</p>
+				<p>I have a more detailed <a href="resume.pdf" target="_blank" rel="noopener noreferrer">résumé</a> if you want to look.</p>
 			</div>
 		);
 	}
